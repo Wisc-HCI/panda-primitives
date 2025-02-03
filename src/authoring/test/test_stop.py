4421c3f21412d5bd126b0630d53d3ab530c73729
kindred-yi <yyi49@wisc.edu>
2025-02-03 14:28:12 -0600

    add some primitives tests

---
+++ src/authoring/test/test_stop.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import copy
+
+import rospy
+from std_msgs.msg import String
+from authoring_msgs.msg import Command, Action 
+from panda_ros_msgs.msg import HybridPose, HybridPoseArray
+
+
+def test_stop():
+    pub = rospy.Publisher('/parser/command', Command, queue_size=1)
+    rate = rospy.Rate(10) # 10hz
+
+    # For some reason, a single message does not go through so need to
+    # send at least 2.
+    for i in range(2):
+
+        hybrid_pose1 = HybridPose()
+        hybrid_pose1.sel_vector = [1,1,1,0,0,0]
+        hybrid_pose1.pose.position.x=0.52 # Forward
+        hybrid_pose1.pose.position.y=0.0  # Sideways
+        hybrid_pose1.pose.position.z=-0.24 # Up
+        
+        # This is facing Downward (x=0, y=0, z=0, w=1)
+        hybrid_pose1.pose.orientation.w=1
+
+        # The mover does something where it transforms based on the constraint frame (quaternion)
+        # To have no transform, the following should be set: x=0, y=0, z=0, w=1
+        hybrid_pose1.constraint_frame.w = 1 
+
+
+        poses = HybridPoseArray()
+        poses.poses = [hybrid_pose1] 
+
+        action1 = Action(type=0,  # PICK
+                        poses=poses, 
+                        item=String(data="BOLT") # NOT SURE IF THIS IS CORRECT
+                        )
+        
+        hybrid_pose2 = HybridPose()
+        hybrid_pose2.sel_vector = [1,1,1,0,0,0]
+        hybrid_pose2.pose.position.x=0.52 # Forward
+        hybrid_pose2.pose.position.y=0.2  # Sideways
+        hybrid_pose2.pose.position.z=-0.38 # Up
+        
+        # This is facing Downward (x=0, y=0, z=0, w=1)
+        hybrid_pose2.pose.orientation.w=1
+
+        # The mover does something where it transforms based on the constraint frame (quaternion)
+        # To have no transform, the following should be set: x=0, y=0, z=0, w=1
+        hybrid_pose2.constraint_frame.w = 1 
+
+
+        poses = HybridPoseArray()
+        poses.poses = [hybrid_pose2] 
+
+
+        action2 = Action(type=2,  # PLACE
+                        poses=poses, 
+                        item=String(data="BOLT") # NOT SURE IF THIS IS CORRECT
+                        )
+        
+
+
+        hybrid_pose3 = HybridPose()
+        hybrid_pose3.sel_vector = [1,1,1,0,0,0]
+        hybrid_pose3.pose.position.x=0.0 # Forward
+        hybrid_pose3.pose.position.y=0.0  # Sideways
+        hybrid_pose3.pose.position.z=0.0 # Up
+        
+        # This is facing Downward (x=0, y=0, z=0, w=1)
+        hybrid_pose3.pose.orientation.w=1
+
+        # The mover does something where it transforms based on the constraint frame (quaternion)
+        # To have no transform, the following should be set: x=0, y=0, z=0, w=1
+        hybrid_pose3.constraint_frame.w = 1 
+
+
+        poses = HybridPoseArray()
+        poses.poses = [hybrid_pose3] 
+
+        action3 = Action(type=14,  # STOP
+                        poses=poses, 
+                        item=String(data="BOLT") # NOT SURE IF THIS IS CORRECT
+                        )
+
+
+        
+        cmd = Command()
+        cmd.type = 2  # EXEC
+        cmd.core_action = [action1, action2, action3]
+
+
+        rospy.loginfo(cmd)
+        pub.publish(cmd)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('test_twist', anonymous=True)
+    
+    test_stop()
+ 
