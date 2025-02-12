56ca82d7ddb302702bd060832dbc15a2a894171d
Mya Schroder <schromya@gmail.com>
2025-02-12 16:21:54 -0600

    Cleaned up test_move_force

--- src/authoring/test/test_move_force.py
+++ src/authoring/test/test_move_force.py
@@ -1,11 +1,11 @@
 """
 Tests MOVE_FORCE primitive. How this works is that the robot
-will move to the x, y position provided (z not allowed) while
-maintaining a constant force (measured when the command triggered).
-If force drops too much, the robot will stop.
+will move to the (x, y)=(0.5, 0.3) position provided (z not allowed) while
+maintaining force of ~7 newtons downwards. This means that the 
+robot will continue moving slowly downwards once it reaches the x,y position
+until an upward force of >7 Newtons is applied by the user (this can be a gentle upward push).
 
 """
-
 
 
 import rospy
@@ -15,70 +15,26 @@ from panda_ros_msgs.msg import HybridPose, HybridPoseArray
 
 
 def test_move_force():
-    pub = rospy.Publisher('/parser/command', Command, queue_size=1)
+    pub = rospy.Publisher('/parser/command', Command, queue_size=1, latch=True)
     rate = rospy.Rate(10) # 10hz
 
-    # First move to position
-    for i in range(2):
-        hybrid_pose = HybridPose()
-        hybrid_pose.sel_vector = [1,1,1,0,0,0]
-        hybrid_pose.pose.position.x=0.5 # Forward
-        hybrid_pose.pose.position.y=0.1  # Sideways
-        hybrid_pose.pose.position.z=-0.22 # Up
-        
-        # This is facing Downward (x=0, y=0, z=0, w=1)
-        hybrid_pose.pose.orientation.w=1
 
-        # The mover does something where it transforms based on the constraint frame (quaternion)
-        # To have no transform, the following should be set: x=0, y=0, z=0, w=1
-        hybrid_pose.constraint_frame.w = 1 
-
-        header = Header()
-        header.stamp = rospy.Time.now() + rospy.Duration()  # 0.5 seconds into the future  # Current time
-        hybrid_pose.header = header
-
-        action = Action(type=6,  # MOVE
-                        pose=hybrid_pose, 
-                        item=String(data="NONE")
-                        )
-
-
-        
-        cmd = Command()
-        cmd.type = 2  # EXEC
-        cmd.core_action = [action]
-
-
-        rospy.loginfo(cmd)
-        pub.publish(cmd)
-        rate.sleep()
-
-  
-    pause = rospy.Rate(1/5) 
-    pause.sleep() # Wait 5 seconds
-
-    # Then test move with force by moving slightly right
-
-
-    # Z position, selection vector, and constraint frame
+    # Z position and selection vector
     # do not matter bc set in mover_server
     hybrid_pose = HybridPose()
     hybrid_pose.pose.position.x=0.5 # Forward
-    hybrid_pose.pose.position.y=0.2  # Sideways
-    hybrid_pose.pose.position.z=-0.23 # Up
-    # hybrid_pose.sel_vector = [1,1,0,0,0,0]
-    
-    # This is facing Downward (x=0, y=0, z=0, w=1)
-    hybrid_pose.pose.orientation.w=1
+    hybrid_pose.pose.position.y=0.3  # Sideways
 
-    # The mover does something where it transforms based on the constraint frame (quaternion)
-    # To have no transform, the following should be set: x=0, y=0, z=0, w=1
+    hybrid_pose.pose.orientation.w=1 # This is facing Downward (x=0, y=0, z=0, w=1)
+
+    # We still need a constraint frame bc although it is set in move_force in mover_server,
+    # mover_server also uses it to set _starting_pose
     hybrid_pose.constraint_frame.w = 1 
 
 
     # Timestamp required
     header = Header()
-    header.stamp = rospy.Time.now() + rospy.Duration(3)  # 0.5 seconds into the future  # Current time
+    header.stamp = rospy.Time.now()
     hybrid_pose.header = header
     
 
